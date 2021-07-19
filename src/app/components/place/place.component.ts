eaa20d68ef2263f511a4862a45d21804ccf4e11f
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2021-07-19 08:53:43 +0300

    SIngle place page: design, persistent id saving

--- src/app/components/place/place.component.ts
+++ src/app/components/place/place.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../services/places.service';
+import { Place } from '../../Place';
 
 @Component({
   selector: 'app-place',
   templateUrl: './place.component.html',
-  styleUrls: ['./place.component.css']
+  styleUrls: ['./place.component.css'],
 })
 export class PlaceComponent implements OnInit {
+  public id: string;
+  public place: Place;
 
-  constructor() { }
+  constructor(private placesService: PlacesService) {}
 
   ngOnInit(): void {
+    if (history.state.id) {
+      this.id = history.state.id;
+      localStorage.setItem('id', history.state.id);
+    } else {
+      this.id = localStorage.getItem('id') || '';
+    }
   }
 
+  onSubmit() {
+    this.placesService
+      .getPlace(this.id)
+      .subscribe((place) => (this.place = place));
+  }
 }
